6a78e9b90343451ae02300f6c2342215e9d3663b
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-12-14 14:35:22 +0500

    Add 2 extra func and PERIOD

--- api/constants.py
+++ api/constants.py
@@ -1,7 +1,9 @@
 import string
+from datetime import timedelta, datetime
 
 MODEL_LEN = 2
 VERSION_LEN = 2
 LETTERS = string.ascii_letters + string.digits
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-
+# for robot summary
+PERIOD = datetime.today() - timedelta(days=7)
--- api/views.py
+++ api/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
-from rest_framework import filters, mixins, permissions, viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import action
-from django.utils import timezone
 import pandas as pd
 from io import BytesIO
 
 from django.db.models import Count
 from robots.models import Robot
 from api.serializers import RobotGetSerializer, RobotPostSerializer
+from api.constants import PERIOD
 
 
-def get_models():
-    """Return all models of robots"""
-    return list(Robot.objects.values_list('model', flat=True)
-                .distinct())
+def get_models(period=PERIOD) -> list[str]:
+    """Возвращает новые за период модели."""
+    new_robot_models = Robot.objects.filter(
+        created__gte=period).values_list('model', flat=True).distinct()
+    return list(new_robot_models)
+
+
+def get_robots_by_model(model: str, period=PERIOD) -> list[str]:
+    """Возвращает созданных за период роботов по модели."""
+    new_robots = Robot.objects.filter(
+        model=model, created__gte=period).values(
+            'model', 'version').annotate(Count('serial')).order_by()
+    return list(new_robots)
 
 
 class RobotViewSet(viewsets.ModelViewSet):
-    """View for robots-crud-operations + download xlsx-file."""
+    """Вью для роботов: crud-операции + скачать Excel-файл."""
     queryset = Robot.objects.all()
 
     def get_serializer_class(self):
@@ -29,18 +38,18 @@ class RobotViewSet(viewsets.ModelViewSet):
     @action(detail=False,)
     def download_summary(self, request):
         """Скачать Excel-файл со сводкой."""
-
         with BytesIO() as b:
             writer = pd.ExcelWriter(b, engine='xlsxwriter')
             for model in get_models():
-                robots = Robot.objects.filter(model=model).values('model', 'version').annotate(dcount=Count('serial')).order_by()
+                robots = get_robots_by_model(model)
                 df = pd.DataFrame(data=list(robots))
-                df.columns = ['Модель', 'Версия', 'Количество за неделю']
+                df.columns = ['Модель', 'Версия', 'Количество']
                 df.to_excel(writer, index=False,
                             sheet_name=f'Модель робота {model}')
             writer.close()
-            filename = 'test'
+            filename = f'Robots created since {PERIOD}'
             content_type = 'application/vnd.ms-excel'
             response = HttpResponse(b.getvalue(), content_type=content_type)
-            response['Content-Disposition'] = 'attachment; filename="' + filename + '.xlsx"'
+            response['Content-Disposition'] = (
+                f'attachment; filename={filename}.xlsx')
             return response
